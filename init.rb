2e3eb615c66eed264750513166324fec26fd6774
Kenner Kliemann <kenner.kliemann@pti.org.br>
2019-07-11 16:03:14 -0300

    remove attr not supported yet

--- init.rb
+++ init.rb
@@ -4,7 +4,6 @@ require 'redmine'
 Redmine::Plugin.register :redmine_monitoring_controlling do
   name 'Redmine (Monitoring & Controlling | Monitoramento & Controle)'
   author 'Alexander Monteiro'
-  contributor 'Kenner Kliemann'
   description "Este plugin foi criado para auxiliar no Monitoramento e Controle dos projetos no redmine através de uma visualização gráfica das tarefas e sua execução.\n
                This plugin is a graphic tool to Monitoring and Controlling projects on redmine."
   version '0.2.1'
